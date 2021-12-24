1f68493fddfbb3e8228f69e37e6c1375540db805
Facebear-ljx <1076231926@qq.com>
2021-12-24 11:26:39 +0800

    fix td3_bc

--- RL_algos/TD3_BC_algos.py
+++ RL_algos/TD3_BC_algos.py
@@ -14,26 +14,6 @@ from Sample_Dataset.Prepare_env import prepare_env
 from Network.Actor_Critic_net import Actor_deterministic, Double_Critic
 import matplotlib.pyplot as plt
 
-ALPHA_MAX = 500.
-ALPHA_MIN = 0.2
-EPS = 1e-8
-
-
-def laplacian_kernel(x1, x2, sigma=20.0):
-    d12 = torch.sum(torch.abs(x1[None] - x2[:, None]), dim=-1)
-    k12 = torch.exp(- d12 / sigma)
-    return k12
-
-
-def mmd(x1, x2, kernel, use_sqrt=False):
-    k11 = torch.mean(kernel(x1, x1), dim=[0, 1])
-    k12 = torch.mean(kernel(x1, x2), dim=[0, 1])
-    k22 = torch.mean(kernel(x2, x2), dim=[0, 1])
-    if use_sqrt:
-        return torch.sqrt(k11 + k22 - 2 * k12 + EPS)
-    else:
-        return k11 + k22 - 2 * k12
-
 
 class TD3_BC:
     def __init__(self,
@@ -64,12 +44,6 @@ class TD3_BC:
 
         self.s_mean, self.s_std = self.replay_buffer.convert_D4RL(self.dataset, scale_rewards=False, scale_state=True)
 
-        # self.env2 = gym.make('hopper-expert-v2')
-        # self.dataset2 = self.env2.get_dataset()
-        # self.replay_buffer2 = ReplayBuffer(state_dim=num_state, action_dim=num_action, device=device)
-        # self.replay_buffer2.convert_D4RL(d4rl.qlearning_dataset(self.env2, self.dataset2), scale_rewards=True,
-        #                                  scale_state=True)
-
         # prepare the actor and critic
         self.actor_net = Actor_deterministic(num_state, num_action, num_hidden, device).float().to(device)
         self.actor_target = copy.deepcopy(self.actor_net)
@@ -91,7 +65,7 @@ class TD3_BC:
         self.policy_noise = policy_noise
         self.policy_freq = policy_freq
         self.explore_freq = explore_freq
-        self.evaluate_freq = 500
+        self.evaluate_freq = 3000
         self.noise_clip = noise_clip
         self.alpha = alpha
         self.batch_size = 256
@@ -112,27 +86,20 @@ class TD3_BC:
 
             # update Critic
             critic_loss_pi = self.train_Q_pi(state, action, next_state, reward, not_done)
-            critic_loss_miu = self.train_Q_miu(state, action, next_state, next_action, reward, not_done)
 
             # delayed policy update
             if self.total_it % self.policy_freq == 0:
-                actor_loss, bc_loss, Q_pi_mean, Q_miu_mean = self.train_actor(state, action)
-                explore_loss, Q_pi_explore, Q_miu_explore = self.train_explorer(state, action)
+                actor_loss, bc_loss, Q_pi_mean = self.train_actor(state, action)
 
-                wandb.log({"actor_loss": actor_loss,
-                           "bc_loss": bc_loss,
-                           "Q_pi_loss": critic_loss_pi,
-                           "Q_miu_loss": critic_loss_miu,
-                           "Q_pi_mean": Q_pi_mean,
-                           "Q_miu_mean": Q_miu_mean,
-                           "Q_pi - Q_miu": Q_pi_mean - Q_miu_mean,
-                           "explore_loss": explore_loss,
-                           "Q_pi_explore": Q_pi_explore,
-                           "Q_miu_explore": Q_miu_explore
-                           })
-
-            if self.total_it % self.evaluate_freq == 0:
-                self.rollout_evaluate()
+                if self.total_it % self.evaluate_freq == 0:
+                    evaluate_reward = self.rollout_evaluate()
+                    wandb.log({"actor_loss": actor_loss,
+                               "bc_loss": bc_loss,
+                               "Q_pi_loss": critic_loss_pi,
+                               "Q_pi_mean": Q_pi_mean,
+                               "evaluate_rewards": evaluate_reward,
+                               "it_steps": self.total_it
+                               })
 
             if self.total_it % 100000:
                 self.save_parameters()
@@ -161,29 +128,29 @@ class TD3_BC:
         self.critic_optim.step()
         return critic_loss.cpu().detach().numpy().item()
 
-    def train_Q_miu(self, state, action, next_state, next_action, reward, not_done):
-        with torch.no_grad():
-            # Compute the target Q value
-            target_Q1, target_Q2 = self.critic_target_miu(next_state, next_action)
-            target_Q = torch.min(target_Q1, target_Q2)
-            target_Q = reward + not_done * self.gamma * target_Q
-
-        Q1, Q2 = self.critic_net_miu(state, action)
-
-        # Critic loss
-        critic_loss = nn.MSELoss()(Q1, target_Q) + nn.MSELoss()(Q2, target_Q)
-
-        # Optimize Critic
-        self.critic_miu_optim.zero_grad()
-        critic_loss.backward()
-        self.critic_miu_optim.step()
-        return critic_loss.cpu().detach().numpy().item()
+    # def train_Q_miu(self, state, action, next_state, next_action, reward, not_done):
+    #     with torch.no_grad():
+    #         # Compute the target Q value
+    #         target_Q1, target_Q2 = self.critic_target_miu(next_state, next_action)
+    #         target_Q = torch.min(target_Q1, target_Q2)
+    #         target_Q = reward + not_done * self.gamma * target_Q
+    #
+    #     Q1, Q2 = self.critic_net_miu(state, action)
+    #
+    #     # Critic loss
+    #     critic_loss = nn.MSELoss()(Q1, target_Q) + nn.MSELoss()(Q2, target_Q)
+    #
+    #     # Optimize Critic
+    #     self.critic_miu_optim.zero_grad()
+    #     critic_loss.backward()
+    #     self.critic_miu_optim.step()
+    #     return critic_loss.cpu().detach().numpy().item()
 
     def train_actor(self, state, action):
         # Actor loss
         action_pi = self.actor_net(state)
         Q_pi = self.critic_net.Q1(state, action_pi)
-        Q_miu = self.critic_net_miu.Q1(state, action_pi)
+        # Q_miu = self.critic_net_miu.Q1(state, action_pi)
 
         lmbda = self.alpha / Q_pi.abs().mean().detach()
 
@@ -200,56 +167,56 @@ class TD3_BC:
         for param, target_param in zip(self.critic_net.parameters(), self.critic_target.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
-        for param, target_param in zip(self.critic_net_miu.parameters(), self.critic_target_miu.parameters()):
-            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+        # for param, target_param in zip(self.critic_net_miu.parameters(), self.critic_target_miu.parameters()):
+        #     target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
         for param, target_param in zip(self.actor_net.parameters(), self.actor_target.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
         return actor_loss.cpu().detach().numpy().item(), \
                bc_loss.cpu().detach().numpy().item(), \
-               Q_pi.mean().cpu().detach().numpy().item(), \
-               Q_miu.mean().cpu().detach().numpy().item()
+               Q_pi.mean().cpu().detach().numpy().item()
+               # Q_miu.mean().cpu().detach().numpy().item()
 
-    def train_explorer(self, state, action):
-        action_explore = self.actor_explore(state)
-        Q_pi = self.critic_net.Q1(state, action_explore)
-        Q_miu = self.critic_net_miu.Q1(state, action_explore)
+    # def train_explorer(self, state, action):
+    #     action_explore = self.actor_explore(state)
+    #     Q_pi = self.critic_net.Q1(state, action_explore)
+    #     Q_miu = self.critic_net_miu.Q1(state, action_explore)
+    #
+    #     Q_explore = Q_pi + (Q_pi - Q_miu) * 0.2
+    #     lmbda = self.alpha / Q_explore.abs().mean()
+    #
+    #     bc_loss = nn.MSELoss()(action, action_explore)
+    #     exploration_loss = -lmbda * Q_explore.mean() + bc_loss
+    #
+    #     # Optimize explorer
+    #     self.actor_explore_optim.zero_grad()
+    #     exploration_loss.backward()
+    #     self.actor_explore_optim.step()
+    #
+    #     return exploration_loss.cpu().detach().numpy().item(), \
+    #            Q_pi.mean().cpu().detach().numpy().item(), \
+    #            Q_miu.mean().cpu().detach().numpy().item()
 
-        Q_explore = Q_pi + (Q_pi - Q_miu) * 0.2
-        lmbda = self.alpha / Q_explore.abs().mean()
-
-        bc_loss = nn.MSELoss()(action, action_explore)
-        exploration_loss = -lmbda * Q_explore.mean() + bc_loss
-
-        # Optimize explorer
-        self.actor_explore_optim.zero_grad()
-        exploration_loss.backward()
-        self.actor_explore_optim.step()
-
-        return exploration_loss.cpu().detach().numpy().item(), \
-               Q_pi.mean().cpu().detach().numpy().item(), \
-               Q_miu.mean().cpu().detach().numpy().item()
-
-    def online_exploration(self, exploration_step=int(1e+5)):
-        state = self.env.reset()
-        for i in range(exploration_step):
-            state_norm = (state - self.s_mean) / (self.s_std + 1e-5)
-            action = self.actor_net(state_norm).cpu().detach().numpy()
-            next_state, reward, done, _ = self.env.step(action)
-
-            state = state.squeeze()
-            action = action.squeeze()
-            next_state = next_state.squeeze()
-
-            # add online sample to replay buffer
-            self.replay_buffer.add_data_to_buffer(state, action, reward, done)
-            state = next_state
-            if done:
-                state = self.env.reset()
-
-        self.dataset = self.replay_buffer.cat_new_dataset(self.dataset)
-        self.s_mean, self.s_std = self.replay_buffer.convert_D4RL(self.dataset, scale_rewards=False, scale_state=True)
+    # def online_exploration(self, exploration_step=int(1e+5)):
+    #     state = self.env.reset()
+    #     for i in range(exploration_step):
+    #         state_norm = (state - self.s_mean) / (self.s_std + 1e-5)
+    #         action = self.actor_net(state_norm).cpu().detach().numpy()
+    #         next_state, reward, done, _ = self.env.step(action)
+    #
+    #         state = state.squeeze()
+    #         action = action.squeeze()
+    #         next_state = next_state.squeeze()
+    #
+    #         # add online sample to replay buffer
+    #         self.replay_buffer.add_data_to_buffer(state, action, reward, done)
+    #         state = next_state
+    #         if done:
+    #             state = self.env.reset()
+    #
+    #     self.dataset = self.replay_buffer.cat_new_dataset(self.dataset)
+    #     self.s_mean, self.s_std = self.replay_buffer.convert_D4RL(self.dataset, scale_rewards=False, scale_state=True)
 
     def rollout_evaluate(self):
         ep_rews = 0.
@@ -261,7 +228,7 @@ class TD3_BC:
             ep_rews += reward
             if done:
                 break
-        wandb.log({"pi_episode_reward": ep_rews})
+        return ep_rews
 
     def save_parameters(self):
         torch.save(self.critic_net_miu.state_dict(), self.file_loc[0])
--- train_td3+bc.py
+++ train_td3+bc.py
@@ -4,7 +4,7 @@ from RL_algos.TD3_BC_algos import TD3_BC
 
 
 def main():
-    wandb.init(project="TD3_BC", entity="facebear")
+    wandb.init(project="test_td3_bc", entity="facebear")
 
     # Parameters
     parser = argparse.ArgumentParser(description='Solve the Hopper-v2 with TD3_BC')
@@ -14,15 +14,14 @@ def main():
     wandb.config.update(args)
 
     # setup mujoco environment and SBAC agent
-    env_name = "hopper-medium-replay-v2"
+    env_name = "hopper-medium-expert-v2"
     agent_TD3_BC = TD3_BC(env_name=env_name,
                           device=args.device,
                           ratio=1
                           )
 
-    for _ in range(10000):
-        agent_TD3_BC.learn(total_time_step=int(1e+6))
-        # agent_TD3_BC.online_exploration(exploration_step=int(1e+3))
+    agent_TD3_BC.learn(total_time_step=int(1e+6))
+    # agent_TD3_BC.online_exploration(exploration_step=int(1e+3))
 
 
 if __name__ == '__main__':
